a5a44aefec8dc410cca14b29911d04465d3453a2
Jan Babák <babakjan@fit.cvut.cz>
2023-04-14 13:46:23 +0200

    Add: serial job file

---
+++ serial_job.sh
@@ -0,0 +1,31 @@
+"serial_job.sh" 30L, 1225C                                                                                                                                30,1          All
+#!/bin/sh
+
+#  ===========================================================================
+# |                                                                           |
+# |             COMMAND FILE FOR SUBMITTING SGE JOBS                          |
+# |                                                                           |
+# |                                                                           |
+# | SGE keyword statements begin with #$                                      |
+# |                                                                           |
+# | Comments begin with #                                                     |
+# | Any line whose first non-blank character is a pound sign (#)              |
+# | and is not a SGE keyword statement is regarded as a comment.              |
+#  ===========================================================================
+
+# Request Bourne shell as shell for job
+#$ -S /bin/sh
+
+# Execute the job from the current working directory.
+#$ -cwd
+
+# Defines  or  redefines  the  path used for the standard error stream of the job.
+#$ -e ./output
+
+# The path used for the standard output stream of the job.
+#$ -o ./output
+
+# Do not change.
+#$ -pe ompi 1
+
+time ./pdp
